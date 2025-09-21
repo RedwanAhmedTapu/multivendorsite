5f56c725351856215ec70098457a0877af87d7cf
root <root@vmi2812441.contaboserver.net>
2025-09-21 07:24:54 +0200

    Resolve merge conflict in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  output: 'export', // enable static HTML export
   images: {
-    domains: ["localhost", "127.0.0.1","api.finixmart.com.bd"],
+    domains: ["localhost", "127.0.0.1", "api.finixmart.com.bd"],
   },
   webpack(config) {
     config.module.rules.push({
@@ -16,3 +17,4 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
