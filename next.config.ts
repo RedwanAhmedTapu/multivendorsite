2bd9c3295c9c473b456c168c56ec620144c2e36e
Redwan Ahmed Tapu <121452952+RedwanAhmedTapu@users.noreply.github.com>
2025-09-21 14:13:04 +0600

    export removed

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  output: 'export', // enable static HTML export
   images: {
     domains: ["localhost", "127.0.0.1", "api.finixmart.com.bd"],
   },
